0073995a2dadd168da428f9a28d2b60c7aa60908
kvo52@kvo1 <kvo.52@mail.ru>
2023-10-19 16:57:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//2.Условные операторы, циклы, массивы, строки, StringBuilder
+//Дан двумерный массив.
+
+//732
+
+//496
+
+//185
+
+//Отсортировать данные в нем по возрастанию.
+
+//123
+
+//456
+
+//789
+
+//Вывести результат на печать.
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+
+        int y = a.GetLength(0);
+        int x = a.GetLength(1);
+        int[] sort = new int[x * y];
+        int counter = 0;
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                sort[counter] = a[i, j];
+                counter++;
+            }
+        }
+        Array.Sort(sort);
+        counter = 0;
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                a[i, j] = sort[counter];
+                counter++;
+                Console.Write(a[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
